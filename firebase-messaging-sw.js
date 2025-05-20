5d19065b3d4af465943895c7007ce9b4dae054c2
Vojta Gryc <vojtech.gryc@knihobot.cz>
2025-05-20 23:46:19 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -6,13 +6,23 @@ firebase.initializeApp({
     apiKey: "AIzaSyDVc8YtOD5Km-RDhGvww7x8WupcNSpWipc",
     authDomain: "get-it-done-52bb1.firebaseapp.com",
     projectId: "get-it-done-52bb1",
+    storageBucket: "get-it-done-52bb1.appspot.com",
     messagingSenderId: "1076164214014",
-    appId: "1:1076164214014:web:483731f66c0d931e9ef9af"
+    appId: "1:1076164214014:web:483731f66c0d931e9ef9af",
+    measurementId: "G-69EG6J8WCD"
 });
 
 const messaging = firebase.messaging();
 
 /* Show the push when it arrives in the background */
-messaging.onBackgroundMessage(({ notification }) => {
-    self.registration.showNotification(notification.title, notification);
+messaging.onBackgroundMessage((payload) => {
+    console.log('Received background message:', payload);
+
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+        body: payload.notification.body,
+        icon: '/icons/icon.svg'
+    };
+
+    self.registration.showNotification(notificationTitle, notificationOptions);
 }); 
